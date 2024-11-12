118f537ed50279ffd427e7cdfbf794388bca02e3
sachin <sachinsebastian12@hotmail.com>
2024-11-12 20:18:43 +0530

    added gradient to floating button

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,23 +11,36 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.vertical(
-          top: Radius.circular(30)
-        ),
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
         child: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          elevation: 3,
-          items: const [
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.graph_square_fill),label: 'Stats')
-          
-        ]),
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            elevation: 3,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.graph_square_fill), label: 'Stats')
+            ]),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: const FloatingActionButton(onPressed: null,
-      shape: CircleBorder(),
-      child: Icon(CupertinoIcons.add),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        child: Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(colors: [
+                  Theme.of(context).colorScheme.tertiary,
+                  Theme.of(context).colorScheme.secondary,
+                  Theme.of(context).colorScheme.primary
+                ],
+                transform: const GradientRotation(pi/4)
+                )),
+                
+            child: const Icon(CupertinoIcons.add)),
       ),
     );
   }
