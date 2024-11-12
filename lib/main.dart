27f96e8a733013942cf525e018f4d4262cb5dd88
sachin <sachinsebastian12@hotmail.com>
2024-11-12 18:52:16 +0530

    intial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker_romain_girou/app.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
