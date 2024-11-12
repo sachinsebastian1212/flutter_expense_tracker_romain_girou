1d5252cec62e45db5c2eeda0f43bc1dce454035d
sachin <sachinsebastian12@hotmail.com>
2024-11-12 19:08:35 +0530

    set basic theme and added home screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker_romain_girou/app_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MyAppView();
   }
 }
---
+++ lib/app_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker_romain_girou/screens/home/views/home_screen.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          surface: Colors.grey.shade100,
+          onSurface: Colors.black,
+          primary: const Color(0xFF00B2E7),
+          secondary: const Color(0xFFE064F7),
+          tertiary: const Color(0xFFFF8D6C)
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
