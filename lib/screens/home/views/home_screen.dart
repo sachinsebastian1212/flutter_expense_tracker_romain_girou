1ed60c29bcd478af8fe20cea8d4539a78c010596
sachin <sachinsebastian12@hotmail.com>
2024-12-17 19:05:16 +0530

    starting with stats screen

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -3,47 +3,55 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker_romain_girou/screens/home/views/main_screen.dart';
+import 'package:flutter_expense_tracker_romain_girou/screens/stats/stats.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
-        child: BottomNavigationBar(
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            elevation: 3,
-            items: const [
-              BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.home), label: 'Home'),
-              BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.graph_square_fill), label: 'Stats')
-            ]),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        shape: const CircleBorder(),
-        child: Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(colors: [
-                  Theme.of(context).colorScheme.tertiary,
-                  Theme.of(context).colorScheme.secondary,
-                  Theme.of(context).colorScheme.primary
-                ],
-                transform: const GradientRotation(pi/4)
-                )),
-                
-            child: const Icon(CupertinoIcons.add)),
-      ),
-      body: const MainScreen(),
-    );
+        bottomNavigationBar: ClipRRect(
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
+          child: BottomNavigationBar(
+              onTap: (value) {
+                setState(() {
+                  index = value;
+                });
+              },
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              elevation: 3,
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(CupertinoIcons.home), label: 'Home'),
+                BottomNavigationBarItem(
+                    icon: Icon(CupertinoIcons.graph_square_fill),
+                    label: 'Stats')
+              ]),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          shape: const CircleBorder(),
+          child: Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(colors: [
+                    Theme.of(context).colorScheme.tertiary,
+                    Theme.of(context).colorScheme.secondary,
+                    Theme.of(context).colorScheme.primary
+                  ], transform: const GradientRotation(pi / 4))),
+              child: const Icon(CupertinoIcons.add)),
+        ),
+        body: index == 0 ? const MainScreen() : const StatsScreen());
   }
 }
